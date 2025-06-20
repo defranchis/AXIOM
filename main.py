1413e5c7eeedfaa46af7569e629ef8dc2f67b593
hstruik <haroldstruik@gmail.com>
2025-06-20 14:12:31 +0200

    all kinds of cleaning up

--- main.py
+++ main.py
@@ -30,51 +30,71 @@ from optparse import OptionParser
 
 
 def main():
-	usage = "usage: prog [options] id test[(parameter=value parameter2=value)]"
 
+	# This sets up the CLI commands, receives potential parameters and sets config paths, if provided. 
+	usage = "usage: prog [options] id test[(parameter=value parameter2=value)]"
 	parser = OptionParser(usage=usage, version="prog 0.01")
 	parser.add_option("-l", "--list-tests", action="store_true", dest="list_tests", default=False,  help="list all avaliable measurements")
 	parser.add_option("-c", "--config", dest="config_file", default=None, help="Path to the YAML configuration file")
 
 	(options, args) = parser.parse_args()
-
 	if len(args) < 1 and not options.list_tests:
 		parser.error("You have to give an identifier. Try '-h' to get more info.")
 
-	if options.list_tests:
-		list_of_tests = []
-		list_of_valid_prefix = ['test', 'msr', 'measurement', 'exp', 'experiment']
+	# if options.list_tests:
+	# 	list_of_tests = []
+	# 	list_of_valid_prefix = ['test', 'msr', 'measurement', 'exp', 'experiment']
 
-		for test_name in os.listdir('measurements'):
-			for prefix in list_of_valid_prefix:
-				if len(prefix) < len(test_name):
-					if test_name[len(prefix)].isdigit():
-						list_of_tests.append(test_name)
+	# 	for test_name in os.listdir('measurements'):
+	# 		for prefix in list_of_valid_prefix:
+	# 			if len(prefix) < len(test_name):
+	# 				if test_name[len(prefix)].isdigit():
+	# 					list_of_tests.append(test_name)
 
-		print("{a:40s} | {b}" .format(a="Test Name", b="Description") )
-		#print("-" * 80)
-		for test_name in sorted(list_of_tests):
-		    #test = getattr(measurements, test_name)
-			test = test_name
-			#print("%-40s | %s" % (test_name, test.__doc__)
-			print("{a:40s}".format(a=test_name ) )
+	# 	print("{a:40s} | {b}" .format(a="Test Name", b="Description") )
+	# 	#print("-" * 80)
+	# 	for test_name in sorted(list_of_tests):
+	# 	    #test = getattr(measurements, test_name)
+	# 		test = test_name
+	# 		#print("%-40s | %s" % (test_name, test.__doc__)
+	# 		print("{a:40s}".format(a=test_name ) )
 
-		return 0
+	# 	return 0
 
+	# if options.list_tests:
+	# 	list_of_tests = []
+	# 	list_of_valid_prefix = ['test', 'msr', 'measurement', 'exp', 'experiment']
+
+	# 	for test_name in dir(measurements):
+	# 		for prefix in list_of_valid_prefix:
+	# 			if len(prefix) < len(test_name):
+	# 				if test_name[len(prefix)].isdigit():
+	# 					list_of_tests.append(test_name)
+
+	# 	print("{a:40s} | {b}" .format(a="Test Name", b="Description") )
+	# 	print ("-" * 80)
+	# 	for test_name in sorted(list_of_tests):
+	# 		test = getattr(measurements, test_name)
+	# 		print("{a:40s}".format(a=test_name ) )
+
+	# 	return 0
+
+
+	# The argument at [0] is now the identifier that is specified for this test session. 
 	id = args[0]
 
 	test_list = []
 	if len(args) > 1:
-		test_list = args[1:]
+		test_list = args[1:] 
 
 	config_path = options.config_file
 
 	for test_name in test_list:
 		print('this is testname', test_name)
 		try:
-			#test = getattr(measurements, test_name)
+			test = getattr(measurements, test_name)
 			# test = testMD_fullSensorMeasurements
-			test = testMD_fullStrip
+			# test = measurements.testMD_fullStrip
 		except AttributeError:
 			print('Unknown Test.')
 			return 1
